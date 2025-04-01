7248e34b5f184688a1fcf75f2a94bb380a3d4bbf
RuslanZaripov <zaripovruslan864@gmail.com>
2025-04-01 16:30:14 +0300

    change addresses

--- src/app.py
+++ src/app.py
@@ -60,7 +60,7 @@ class WebApp:
         
         # Initialize Kafka
         self.kafka_producer = KafkaProducer(
-            bootstrap_servers="broker:9092",
+            bootstrap_servers="localhost:9092",
             topic="predictions",
         )
         
