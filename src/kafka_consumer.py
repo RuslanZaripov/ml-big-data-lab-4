a7a85f2ce6c082f2130786389221425b7875e8b2
RuslanZaripov <zaripovruslan864@gmail.com>
2025-04-02 19:39:22 +0300

    load str input data

--- src/kafka_consumer.py
+++ src/kafka_consumer.py
@@ -66,7 +66,7 @@ class KafkaConsumer:
     
     def process(self, data: dict):
         prediction_id = data['prediction_id']
-        input_data = data['input_data'] 
+        input_data = json.loads(data['input_data']) 
             
         X = self.scaler.transform(pd.json_normalize(input_data['X']))
         pred = self.model.predict(X).tolist()
